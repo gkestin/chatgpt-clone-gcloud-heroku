748d473ab27874905cdcbe107067f033f6da7358
gkestin <greg.kestin@gmail.com>
2023-06-04 02:49:21 +0100

    Changed run.py to listen to the PORT environment variable

--- run.py
+++ run.py
@@ -1,27 +1,33 @@
-from server.app     import app
+from server.app import app
 from server.website import Website
 from server.backend import Backend_Api
 
 from json import load
+import os
 
 if __name__ == '__main__':
     config = load(open('config.json', 'r'))
     site_config = config['site_config']
-    
+
+    # Update the site_config port if PORT environment variable is set
+    port = os.getenv('PORT')
+    if port:
+        site_config['port'] = int(port)
+
     site = Website(app)
     for route in site.routes:
         app.add_url_rule(
             route,
-            view_func = site.routes[route]['function'],
-            methods   = site.routes[route]['methods'],
+            view_func=site.routes[route]['function'],
+            methods=site.routes[route]['methods'],
         )
 
-    backend_api  = Backend_Api(app, config)
+    backend_api = Backend_Api(app, config)
     for route in backend_api.routes:
         app.add_url_rule(
             route,
-            view_func = backend_api.routes[route]['function'],
-            methods   = backend_api.routes[route]['methods'],
+            view_func=backend_api.routes[route]['function'],
+            methods=backend_api.routes[route]['methods'],
         )
 
     print(f"Running on port {site_config['port']}")
